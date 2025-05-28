955ea59bd968ff3ecbed7e49f17cb5cdfb746042
MusTalK <49585280+mustalk@users.noreply.github.com>
2025-05-28 14:08:51 +0200

    feat: initial Hilt setup and dependency upgrades (#2)
    - Setup an initial basic Hilt configuration.
    - Switched to `ksp` instead of `kapt` for the Hilt compiler
    - Updated Gradle wrapper to version 8.14.1
    - Updated Kotlin version to 2.1.21
    - Updated Android Gradle Plugin version to 8.10.0
    - Enabled Gradle build caching and configuration caching in `gradle.properties`

---
+++ app/src/main/kotlin/com/mustalk/seat/marsrover/MarsRoverApplication.kt
@@ -0,0 +1,7 @@
+package com.mustalk.seat.marsrover
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MarsRoverApplication : Application()
---
+++ app/src/main/kotlin/com/mustalk/seat/marsrover/core/di/AppModule.kt
@@ -0,0 +1,21 @@
+package com.mustalk.seat.marsrover.core.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+    @Provides
+    @Singleton
+    fun provideJson(): Json =
+        Json {
+            ignoreUnknownKeys = true
+            prettyPrint = true
+            isLenient = true
+        }
+}
